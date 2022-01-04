a5041714354ea14ebb02f50bb77c739fffac2cb5
Charlie.O <odadacharles@gmail.com>
2022-01-04 12:09:53 +0300

    Vibration data is now collected and segregated as required. Next step is to attach the vibration sensors to the 3D Printer.

--- get_vibration_data.py
+++ get_vibration_data.py
@@ -11,11 +11,12 @@ serialPort = serial.Serial(port = "COM7", baudrate=115200,
 
 serialPort.close() #close the serial port to end other instances that may still be running in the background
 serialPort.open() #Open the serial port
-readings = 0
-#time.sleep(10) #Wait for 10 seconds
+readings = 0 #This is a variable to set the number of readings to be taken before the program exits. 
+             #This is necessary if the data is to be handled by excel which can't handle csv files with more than 1000000 rows
 
-while (readings<1000): 
-    if(serialPort.in_waiting>0):
+#The number of readings is altered based on the time needed to take the readings. Each sensor generates 81 readings per second. 
+while (readings<2000): 
+    if(serialPort.in_waiting>0): #Check if there's any data from the specified serial port
         f = open('C:/Users/Charlie.O/Documents/Python Projects/3D Printer Vibration Data/Vibrations.csv', 'a',newline='') #open the csv file that will be written to. 'a' worked, 'w' did not
         writer=csv.writer(f) #create an instance of the csv writer that will be writing to the file 'f'
         Time = datetime.now() #Get the current date and time
@@ -23,13 +24,13 @@ while (readings<1000):
         #Read data out of the buffer until a carriage return/new line is found and save to the variable "Temperature"
         
         raw_data=serialPort.readline()  #Convert the input from the serial port to string and save to a variable
-        data_string = raw_data.decode("utf-8")
-        data_list = data_string.split()
-        Dataline.append(UnixTime)
-        Dataline.extend(data_list)
-        print(Dataline)
+        data_string = raw_data.decode("utf-8") #Decode the data string. This helps to remove some miscellaneous information
+        data_list = data_string.split() #Split the string into a list of individual words. This separates data from different vibration axes
+        Dataline.append(UnixTime) #Add the Unixtime to the Dataline list. Time in the unix format keeps the data 'neat' i.e. easier to transfer
+        Dataline.extend(data_list) #Add the data_list list to the Dataline List
+        print(Dataline) #Print the dataline. Helps to verify the data that will be added to the csv file
         writer.writerows([Dataline]) #Write the values of Dataline list to the csv file as a list of lists. A simple list will result in each character being separated by a comma
         Dataline=[] #Reinitialize the dataline list in preparation for the next reading
-        readings+=1
+        readings+=1 #Adds one to the counter
 
-serialPort.close()
+serialPort.close() #close the serial port
--- preprocessing_segregation.py
+++ preprocessing_segregation.py
@@ -1,26 +1,39 @@
 import csv
 
 def main ():
-    csv_file = open('C:/Users/Charlie.O/Documents/Python Projects/3D Printer Vibration Data/Vibrations.csv')
-    csvreader = csv.reader(csv_file)
-    header = next(csvreader)                     #Assign the first item in trading pairs the header variable
+    csv_file = open('C:/Users/Charlie.O/Documents/Python Projects/3D Printer Vibration Data/Vibrations.csv') #Open the csv file where the bulk sensor data is stored
+    csvreader = csv.reader(csv_file) #Read through the bulk csv file and store its contents in a variable called 'csvreader'
 
+    f = open('C:/Users/Charlie.O/Documents/Python Projects/3D Printer Vibration Data/Sensor_1.csv', 'a',newline='', encoding = 'utf-8') #open the csv file that will be written to. 'a' worked, 'w' did not
+    writer=csv.writer(f) #create an instance of the csv writer that will be writing to the file 'f'
+    writer.writerow([" Time ","Sensor", "X-Axis", "Y-Axis", "Z-Axis"]) #Write the header of the csv file
+
+    f = open('C:/Users/Charlie.O/Documents/Python Projects/3D Printer Vibration Data/Sensor_2.csv', 'a',newline='', encoding = 'utf-8') #open the csv file that will be written to. 'a' worked, 'w' did not
+    writer=csv.writer(f) #create an instance of the csv writer that will be writing to the file 'f'
+    writer.writerow([" Time ","Sensor", "X-Axis", "Y-Axis", "Z-Axis"]) #Write the header of the csv file
+
+    f = open('C:/Users/Charlie.O/Documents/Python Projects/3D Printer Vibration Data/Sensor_3.csv', 'a',newline='', encoding = 'utf-8') #open the csv file that will be written to. 'a' worked, 'w' did not
+    writer=csv.writer(f) #create an instance of the csv writer that will be writing to the file 'f'
+    writer.writerow([" Time ","Sensor", "X-Axis", "Y-Axis", "Z-Axis"]) #Write the header of the csv file
+
+
+    #The sensor data is labelled to distinguish inputs from the three sensors. The loop below will go through each row of data and separate the data from the different sensors and store
+    #each sensor's data in its respective csv file.
     for row in csvreader:
-        if "S1" in row[1]:
+        if "S1" in row[1]: 
             f = open('C:/Users/Charlie.O/Documents/Python Projects/3D Printer Vibration Data/Sensor_1.csv', 'a',newline='', encoding = 'utf-8') #open the csv file that will be written to. 'a' worked, 'w' did not
             writer=csv.writer(f) #create an instance of the csv writer that will be writing to the file 'f'
-            new_row = []
-            new_row.append(row[0])
-            new_row.extend(row[1])
-            print(new_row)
-            writer.writerows([new_row])
+            writer.writerows([row])
         elif "S2" in row[1]:
             f = open('C:/Users/Charlie.O/Documents/Python Projects/3D Printer Vibration Data/Sensor_2.csv', 'a',newline='', encoding = 'utf-8') #open the csv file that will be written to. 'a' worked, 'w' did not
             writer=csv.writer(f) #create an instance of the csv writer that will be writing to the file 'f'
-            new_row = []
-            new_row.append(row[0])
-            new_row.extend(row[1])
-            writer.writerows([new_row])
+            writer.writerows([row])
+        elif "S3" in row[1]:
+            f = open('C:/Users/Charlie.O/Documents/Python Projects/3D Printer Vibration Data/Sensor_3.csv', 'a',newline='', encoding = 'utf-8') #open the csv file that will be written to. 'a' worked, 'w' did not
+            writer=csv.writer(f) #create an instance of the csv writer that will be writing to the file 'f'
+            writer.writerows([row])
+        else:
+            pass
 
 #Run the function above if the condition below is met
 if __name__ == "__main__":
